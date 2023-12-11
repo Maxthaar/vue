edc5190c34acc849684455ca17414b5a27e665f7
Maxthaar <153499636+Maxthaar@users.noreply.github.com>
2023-12-11 11:24:24 +0100

    Add files via upload

---
+++ components/CustomerList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- <h2>Onze gewaardeerde klanten</h2> -->
+        <ul>
+            <!-- <li>Microsoft</li>
+            <li>Google</li>
+            <li>Facebook</li>
+            <li>Twitter</li> -->
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomerList'
+}
+</script>
+
+<style scoped>
+div{
+    text-align: left;
+}
+</style>
+  
---
+++ components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+      <p>Hello World!</p>
+      <CustomerList />
+  </div>
+</template>
+
+<script>
+import CustomerList from "./CustomerList";
+
+export default {
+  name: 'HelloWorld',
+  components: {CustomerList},
+  props: {
+    msg: String
+  }
+}
+</script>
+<style>
+body{
+  background: rgba(71,94,108,1);
+}
+h1{
+  color: rgba(1,170,162,1);
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
